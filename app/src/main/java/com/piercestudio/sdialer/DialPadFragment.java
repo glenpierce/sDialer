aaa2c378d1f6ec838c98bf371b2e23053b6a3813
glenpierce <glen.piercev@gmail.com>
2015-09-15 16:33:11 -0700

    keypad[i] is always null. See DialPadFragment line 40.

--- app/src/main/java/com/piercestudio/sdialer/DialPadFragment.java
+++ app/src/main/java/com/piercestudio/sdialer/DialPadFragment.java
@@ -18,81 +18,78 @@ import android.util.Log;
 
 import org.w3c.dom.Text;
 
-public class DialPadFragment extends Fragment
-{
+public class DialPadFragment extends Fragment {
+
+	TextView phoneNumber;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState)
-	{
+							 Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		LinearLayout mlinearlayout = new LinearLayout(getActivity());
 //		mlinearlayout getView().findViewById(R.id.linearlayout);
+
+//		final TextView phoneNumber = (TextView) container.findViewById(R.id.phonenumber);
 		final TextView phoneNumber = (TextView) container.findViewById(R.id.phonenumber);
 
 
 		final ImageButton keyPad[] = new ImageButton[10];
 
-		for (int i = 0; i <= 9; i++)
-		{
-			Log.i("asdf", Integer.toString(i));
+		for (int i = 0; i <= 9; i++) {
 			keyPad[i] = (ImageButton) container.findViewWithTag(Integer.toString(i));
+			if(keyPad[i] == null)
+				Log.i("asdf", "keypad" + Integer.toString(i) + " is null");
 			keyPad[i].setOnClickListener(new View.OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
 					phoneNumber.setText(phoneNumber.getText().toString() + v.getTag().toString());
 				}
-		});
+			});
 		}
 
 		ImageButton deleteButton = (ImageButton) container.findViewById(R.id.deletebutton);
 		deleteButton.setOnClickListener(new View.OnClickListener() {
 
-				@Override
-				public void onClick(View v)
-				{
-					phoneNumber.setText("");
-				}
+			@Override
+			public void onClick(View v) {
+				phoneNumber.setText("");
+			}
 
 		});
 
 		ImageButton dialButton = (ImageButton) container.findViewById(R.id.dialbutton);
 		dialButton.setOnClickListener(new View.OnClickListener() {
 
-				@Override
-				public void onClick(View v)
-				{
-					startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber.getText())));
-				}
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber.getText())));
+			}
 		});
 
 		return inflater.inflate(R.layout.dialpadlayout, container, false);
 	}
 
+//	public TextView getPhoneNumber(){
+//		return phoneNumber;
+//	}
 
-
-
-
-//	OnClickListener keyPress = new OnClickListener()
-//	{
-//		public void onClick(View v)
-//		{
-//			if (v.getTag().equals("deletebutton"))
-//			{
-//				phoneNumber.setText("");
-//			}else
-//			{
-//			if (v.getTag().equals("dialbutton"))
-//			{
-//				startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber.getText())));
-//			}else {
-////				if (phoneNumber.getText().toString().equals(getString(R.string.phonenumber)))
-////				{
-////					phoneNumber.setText(v.getTag().toString());
-////				}else {
-//					phoneNumber.setText(phoneNumber.getText().toString() + v.getTag().toString());
-////				}
-//				}
+	OnClickListener keyPress = new OnClickListener() {
+		public void onClick(View v) {
+			if (v.getTag().equals("deletebutton")) {
+				phoneNumber.setText("");
+			} else {
+				if (v.getTag().equals("dialbutton")) {
+					startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber.getText())));
+				} else {
+					if (phoneNumber.getText().toString().equals(getString(R.string.phonenumber))) {
+						phoneNumber.setText(v.getTag().toString());
+					} else {
+						phoneNumber.setText(phoneNumber.getText().toString() + v.getTag().toString());
+					}
+				}
 			}
+		}
+	};
+}
