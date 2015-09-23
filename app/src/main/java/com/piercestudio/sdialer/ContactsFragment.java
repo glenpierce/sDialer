f03a539d1eed8ac66117e9c917fbe1eedbe51bb4
Glen Pierce <glen.piercev@gmail.com>
2015-09-22 17:55:23 -0700

    Gathering Contacts for ListView In progress.

--- app/src/main/java/com/piercestudio/sdialer/ContactsFragment.java
+++ app/src/main/java/com/piercestudio/sdialer/ContactsFragment.java
@@ -1,23 +1,54 @@
 package com.piercestudio.sdialer;
 
-    import android.app.Fragment;
-    import android.os.Bundle;
-    import android.view.View;
-    import android.view.ViewGroup;
-    import android.view.LayoutInflater;
+import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.ListView;
+import android.database.Cursor;
+import java.net.URL;
+import android.content.ContentResolver;
+import android.provider.ContactsContract;
+import android.content.Context;
 
-public class ContactsFragment extends Fragment{
-
-        View v;
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            v = inflater.inflate(R.layout.contacts_layout, container, false);
-
+import java.util.ArrayList;
 
 
-            return v;
+public class ContactsFragment extends Fragment
+{
+
+    ArrayList<String> contacts;
+    ContactsAdapter adapter;
+    ContentResolver contentResolver;
+    Cursor cursor;
+    View v;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        v = inflater.inflate(R.layout.contacts_list_layout, container, false);
+        ListView listView = (ListView) v.findViewById(R.id.contacts_list_layout_id);
+
+        
+
+        contentResolver = getActivity().getContentResolver();
+        cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                                       null, null, null, null);
+        while (cursor.moveToNext()) {
+            String name = cursor
+                    .getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
         }
+        cursor.close();
+
+
+
+
+        return v;
     }
+
+}
